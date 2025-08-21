5299885f0ae22f0717c54408c992529469f64a7b
relusc <r.schach96@gmail.com>
2025-08-21 13:14:35 +0200

    Push current state

--- containerdays-2025/demo/setup/setup.sh
+++ containerdays-2025/demo/setup/setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+set -euo pipefail
+
+# Stop colima on failure
+cleanup() {
+    colima stop
+}
+
+trap cleanup ERR
+
 # Start colima for docker
 colima start
 
 # Create kind cluster
-kind create cluster --config=./kind.yaml 
+kind create cluster --config=./demo/setup/kind.yaml 
 
 # Complex setup: install Kyverno via Helm
 helm repo add kyverno https://kyverno.github.io/kyverno/
