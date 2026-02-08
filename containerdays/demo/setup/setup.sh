75fbb401520f5c9419735ddf16a042827093c1c7
relusc <r.schach96@gmail.com>
2026-02-08 17:13:17 +0100

    Update docs

--- containerdays/demo/setup/setup.sh
+++ containerdays/demo/setup/setup.sh
@@ -17,13 +17,10 @@ colima start
 kind create cluster --config=./setup/kind.yaml 
 
 # Complex setup: Install Kyverno
+# Documentation about verifying images with Kyverno: https://kyverno.io/policies/other/verify-image/verify-image/
 helm repo add kyverno https://kyverno.github.io/kyverno/
 helm repo update
 helm install kyverno kyverno/kyverno -n kyverno --create-namespace
 
 # Login to GitHub Container Registry (make sure to set or provide working credentials for your account)
 docker login ghcr.io
-
-# Label default namespace so it's being watched by the sigstore policy controller
-# This is (as of 07.02.2026p) required according to https://docs.sigstore.dev/policy-controller/overview/
-kubectl label ns default policy.sigstore.dev/include=true
